e0126b7b0d1e6e1953f4ff25410b409201247e7e
Seu nome de usuario do github <laisllopess21@gmail.com>
2025-11-26 21:15:10 -0300

    criando compenente tela-inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: 'tela-inicial', component: TelaInicialComponent }
+];
